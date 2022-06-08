f1b8d301947eadc0d481c15307e284186fcd7248
Davidkin Dmitrij <dmitr0503@inbox.ru>
2022-06-08 11:32:45 +0300

    2

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -17,13 +17,29 @@ class Post(models.Model):
   updated = models.DateTimeField(auto_now=True)
   status = models.CharField(max_length=2,choices=Status.choices,default=Status.DRAFT)
   objects = models.Manager() # The default manager.
-  published = PublishedManager() # Our custom manager.
+  published = PublishedManager() # Our custom manager.class 
+
   class Meta:
     ordering = ['-publish']
     indexes = [
-       models.Index(fields=['-publish']),
+       models.Index(fields=['-publish']), 
     ]
   def __str__(self):
     return self.title
+class Comment(models.Model):
+  post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+  name = models.CharField(max_length=80)
+  email = models.EmailField()
+  body = models.TextField()
+  created = models.DateTimeField(auto_now_add=True)
+  updated = models.DateTimeField(auto_now=True)
+  active = models.BooleanField(default=True)
+class Meta:
+  ordering = ['created']
+  indexes = [
+  models.Index(fields=['created']),
+    ]
+def __str__(self):
+  return f'Comment by {self.name} on {self.post}'
 
 # Create your models here.
